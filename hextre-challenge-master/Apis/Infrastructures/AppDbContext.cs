acf4156e513d0f38ea8aeda2014861d5c526a86c
janemyn29 <tramygeo@gmail.com>
2023-09-02 02:08:02 +0700

    extend identityUser

--- hextre-challenge-master/Apis/Infrastructures/AppDbContext.cs
+++ hextre-challenge-master/Apis/Infrastructures/AppDbContext.cs
@@ -5,7 +5,7 @@ using Microsoft.EntityFrameworkCore.Design;
 
 namespace Infrastructures
 {
-    public class AppDbContext : IdentityDbContext
+    public class AppDbContext : IdentityDbContext<ApplicationUser>
     {
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
         {
